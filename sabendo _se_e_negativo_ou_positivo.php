2802e372342123718f0055fcdcfb6c8890b79d2f
DougVikt-99 <106497954+DougVikt@users.noreply.github.com>
2022-08-03 15:09:24 -0300

    Create sabendo _se_e_negativo_ou_positivo.php

---
+++ sabendo _se_e_negativo_ou_positivo.php
@@ -0,0 +1,10 @@
+/*========================================== saber se o numero e negativo ou positivo =============================================================*/
+$valor=-34;
+echo "voce colocou o valor $valor ";
+if ($valor>0){
+    echo ", ele é positivo !";
+}
+else{
+    echo ", ele é negativo !";
+}
+
